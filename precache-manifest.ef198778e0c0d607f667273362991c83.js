746ada60e88639b733092dab2dfc2e465964bbdc
tuanthanh.dev <thanhlt1@appota.com>
2020-12-30 00:29:18 +0700

    Updates

---
+++ precache-manifest.ef198778e0c0d607f667273362991c83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8877e64e35f1f972cfb900feb63a52d3",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "b1b7b67decd70f822909",
+    "url": "/weather-app-react/static/css/main.30e3031e.chunk.css"
+  },
+  {
+    "revision": "39aa2121324a49ab51b7",
+    "url": "/weather-app-react/static/js/2.012668d5.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/weather-app-react/static/js/2.012668d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b7b67decd70f822909",
+    "url": "/weather-app-react/static/js/main.ea7c2ea2.chunk.js"
+  },
+  {
+    "revision": "73ec9f2eed65a5d44827",
+    "url": "/weather-app-react/static/js/runtime-main.4ee40ab0.js"
+  },
+  {
+    "revision": "6b70656362d1cf05a9dc5e25c36e4a4f",
+    "url": "/weather-app-react/static/media/bg.6b706563.jpg"
+  }
+]);
